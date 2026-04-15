63d1881293c3f1cddf9584a41449f14f421592fe
Kjames3 <bestofkamren@gmail.com>
2026-04-14 17:14:58 -0700

    test to see if the foxglove server is even running

---
+++ scripts/fgtest.py
@@ -0,0 +1,8 @@
+import asyncio, websockets
+
+async def t():
+    async with websockets.connect('ws://localhost:8765', subprotocols=['foxglove.websocket.v1']) as ws:
+        msg = await ws.recv()
+        print('OK:', str(msg)[:200])
+
+asyncio.run(t())
